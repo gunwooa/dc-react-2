9e2ff9d6029a5e97526d583f9f583389672616ca
gunwooa <gunwoooa@naver.com>
2020-12-20 00:00:59 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  const [videos, setVideos] = useState([]); // 함수형 컴포넌트의 state 선언 방법
+
+  useEffect(() => {
+    console.log('useEffect');
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDIw57i69CFDJbaWBB7Ql7iL7c6uapDUZg",
+      requestOptions
+    )
+      .then(response => response.json()) // 응답을 json으로 변환
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
+  }, []); // 두번 째 인자로 빈배열을 주면 컴포넌트가 마운트 될 때 한 번만 호출
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Videoitem = props => <h3>{props.video.snippet.title}</h3>
+
+export default Videoitem;
